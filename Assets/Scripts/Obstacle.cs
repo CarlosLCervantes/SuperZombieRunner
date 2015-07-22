67f2548c278a9bde7a482a55007a96c05d155311
Carlos Cervantes <CarlosLCervantes@gmail.com>
2015-07-21 22:26:00 -0700

    Collider resize

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,10 +5,19 @@ public class Obstacle : MonoBehaviour, IRecycle {
 
 	public Sprite[] sprites;
 
+	public Vector2 colliderOffset = Vector2.zero;
+
 	public void Restart() {
-		Debug.Log ("asdasdasdad");
 		var renderer = GetComponent<SpriteRenderer> ();
 		renderer.sprite = sprites[Random.Range (0, sprites.Length)];
+
+		var collider = GetComponent<BoxCollider2D> ();
+		var size = renderer.bounds.size;
+		size.y = collider.size.y;
+
+		collider.size = size;
+		collider.offset = new Vector2 (-colliderOffset.x, collider.size.y / 2 - colliderOffset.y);
+
 	}
 
 	public void Shutdown() {
