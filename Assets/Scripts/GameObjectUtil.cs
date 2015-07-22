499605a1dc3289270b87ce645174dbbf8aa28c6d
Carlos Cervantes <CarlosLCervantes@gmail.com>
2015-07-21 21:42:11 -0700

    Object Pool

---
+++ Assets/Scripts/GameObjectUtil.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameObjectUtil {
+
+	private static Dictionary<RecycleGameObject, ObjectPool> pools = new Dictionary<RecycleGameObject, ObjectPool> ();
+
+	public static GameObject Instantiate(GameObject prefab, Vector3 pos) {
+		GameObject instance = null;
+
+		var recycledScript = prefab.GetComponent<RecycleGameObject> ();
+
+		if (recycledScript != null) {
+			var pool = getObjectPool(recycledScript);
+			instance = pool.NextObject(pos).gameObject;
+		} else {
+			instance = GameObject.Instantiate (prefab);
+			instance.transform.position = pos;
+		}
+
+		return instance;
+	}
+
+	public static void Destroy(GameObject gameObject) {
+
+		var recycleGameObject = gameObject.GetComponent<RecycleGameObject> (); //won't throw error if doesn't exist
+
+		if (recycleGameObject == null) {
+			GameObject.Destroy (gameObject);
+		} else {
+			recycleGameObject.ShutDown();
+		}
+	}
+
+	private static ObjectPool getObjectPool(RecycleGameObject reference) {
+		ObjectPool pool = null;
+		if (pools.ContainsKey (reference)) {
+			pool = pools [reference];
+		} else {
+			var poolContainer = new GameObject(reference.gameObject.name + "ObjectPool");
+			pool = poolContainer.AddComponent<ObjectPool>();
+			pool.prefab = reference;
+			pools.Add(reference, pool);
+		}
+
+		return pool;
+	}
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour : IRecycle {
+
+	public void Restart() {
+
+	}
+
+	public void Shutdown() {
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
