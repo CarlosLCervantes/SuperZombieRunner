d9c2e45e70953068838349c67f5fe215a1a68c55
Carlos Cervantes <CarlosLCervantes@gmail.com>
2015-08-02 22:46:19 -0700

    Player model working

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	public float jumpSpeed = 240;
+
+	private Rigidbody2D body2d;
+	private InputState inputState;
+
+	void Awake() {
+		body2d = GetComponent<Rigidbody2D> ();
+		inputState = GetComponent<InputState> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (inputState.standing) {
+			if(inputState.actionButton) {
+				body2d.velocity = new Vector2(0, jumpSpeed);
+			}
+		}
+	
+	}
+}
---
+++ Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimatorManager : MonoBehaviour {
+
+	private Animator animator;
+	private InputState inputState;
+
+	void Awake() {
+		animator = GetComponent<Animator> ();
+		inputState = GetComponent<InputState> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var running = true;
+
+		string msg = inputState.absVelX + " / " + inputState.absVelY + " / " + inputState.standingThreshold;
+		Debug.Log (msg);
+		if (inputState.absVelX > 0 && inputState.absVelY < inputState.standingThreshold) {
+			running = false;
+		}
+
+		animator.SetBool ("Running", running);
+	
+	}
+}
